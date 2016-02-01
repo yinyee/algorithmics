dfda1e42ce9bc99572503db63d08a74a4833b303
yinyee <kanyinyee@gmail.com>
2016-02-01 14:06:57 +0000

    Initial commit

---
+++ task3/LinearLUTM2F.java
@@ -0,0 +1,25 @@
+package task3;
+
+public class LinearLUTM2F {
+
+	public void insert(String string, Integer integer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public char[] retrieve(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void update(String string, Integer integer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remove(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ task3/LinearLUTM2FTest.java
@@ -0,0 +1,38 @@
+package task3;
+
+public class LinearLUTM2FTest {
+	
+	public static void main(String[] args) {
+		try {
+		    LinearLUTM2F myLUT = new LinearLUTM2F();
+		    
+		    myLUT.insert("Priscilla", new Integer(41));
+		    myLUT.insert("Travis", new Integer(34));
+		    myLUT.insert("Samuel", new Integer(28));
+		    myLUT.insert("Helena", new Integer(39));
+		    myLUT.insert("Andrew", new Integer(14));
+		    myLUT.insert("Kay", new Integer(24));
+		    myLUT.insert("John", new Integer(67));	    
+		    System.out.println(myLUT);
+
+		    System.out.println(myLUT.retrieve("Helena"));
+		    System.out.println(myLUT);
+		    
+		    System.out.println(myLUT.retrieve("John"));
+		    System.out.println(myLUT);
+		    
+		    System.out.println(myLUT.retrieve("Travis"));
+		    System.out.println(myLUT);
+
+		    myLUT.update("Samuel", new Integer(29));
+		    System.out.println(myLUT);
+
+		    myLUT.remove("Andrew");
+		    System.out.println(myLUT);
+		
+		} catch (Exception e) {
+		    System.out.println(e);
+		}
+    }
+
+}
